3c94315c03b56d5a0325ee39ef3e1bf50576f3ea
Hovkic <hovkic15@gmail.com>
2022-05-16 17:38:28 -0700

    Try constructor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+const button = document.querySelectorAll('.button');
+const display = document.querySelector('.display');
+const upperDisplay = document.querySelector('.upper-display');
+const digit = document.querySelectorAll('.digit');
+const clear = document.querySelectorAll('.clear');
+const operator = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+class Calculator {
+  constructor(main, upper) {
+    this.display = main;
+    this.upperDisplay = upper;
+    this.clear();
+  }
+
+  clear() {
+    (this.main = ''), (this.upper = '');
+  }
+
+  addDigit(number) {
+    this.main = this.main.toString() + number.toString();
+  }
+
+  update() {
+    this.display.innerText = this.main;
+  }
+}
+
+const calculator = new Calculator(display, upperDisplay);
+
+digit.forEach((digit) => {
+  digit.addEventListener('click', () => {
+    calculator.addDigit(digit.innerText);
+    calculator.update();
+  });
+});
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -8,7 +45,7 @@ const divide = (a, b) => a / b;
 
 const operate = (a, b, operator) => {
   if (operator === '+') {
-    return add(a, b);
+    console.log(add(a, b));
   }
   if (operator === '-') {
     return subtract(a, b);
@@ -20,19 +57,3 @@ const operate = (a, b, operator) => {
     return divide(a, b);
   }
 };
-
-const display = document.querySelector('.main-display');
-const digit = document.querySelectorAll('.digit');
-const clear = document.querySelector('.clear-all');
-const operator = document.querySelectorAll('.operator');
-
-const bank = [];
-
-digit.forEach((digit) => {
-  digit.addEventListener('click', () => {
-    bank.push(digit.textContent);
-    let newbank = bank.join('');
-    newbank = Number(newbank);
-    display.textContent = newbank;
-  });
-});
