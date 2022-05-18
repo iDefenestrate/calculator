cde3d8e5c76ed31ce4203c58e164324674aa7ed7
Hovkic <hovkic15@gmail.com>
2022-05-18 16:21:15 -0700

    Add global variables

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-
-
+const button = document.querySelectorAll('button');
+const digit = document.querySelectorAll('.digit');
+const operator = document.querySelectorAll('.operator');
+const upperDisplay = document.querySelector('.upper-Display');
+const lowerDisplay = document.querySelector('.lower-Display');
+const clear = document.querySelector('.clear');
+const clearAll = document.querySelector('.clear-all');
+const deleteBtn = document.querySelector('.delete');
